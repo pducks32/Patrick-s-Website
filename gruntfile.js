5e65d9a8e89610f5ffc0beb54422f400d30d6178
Patrick Metcalfe <git@patrickmetcalfe.com>
2013-12-20 22:30:52 -0600

    I'm Kind of running out of things to say, I'm just working on it

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    
+	
+	grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+                tasks: ['compass:dev']
+            },
+            livereload: {
+                files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+		compass: {
+		      dev: {
+		        options: {
+		          config: 'config.rb',
+				  quiet: true,
+		          force: true,
+		        }
+		      }
+		    }
+    });
+    grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
